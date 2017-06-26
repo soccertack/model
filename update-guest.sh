42330d918f39944fd6a8ad0450584f8cc0f1557d
Jintack Lim <jintack.lim@linaro.org>
2017-06-26 09:05:12 -0500

    Update guest local
    Signed-off-by: Jintack Lim <jintack.lim@linaro.org>

--- update-guest.sh
+++ update-guest.sh
@@ -6,19 +6,26 @@ source update-common.sh
 #DIR="psci-guest-4.12"
 
 #This is 4.10 with 100Hz
-DIR="pv-guest-model"
+#DIR="pv-guest-model"
+
+LOCAL="/users/jintackl"
+DIR="guest-pv"
+BOOT="arch/arm64/boot"
 
 #This is 4.12
 #DIR="vanilla"
 
-SRC_PATH=$MODEL/$DIR/arch/arm64/boot
+#SRC_PATH=$MODEL/$DIR/arch/arm64/boot
+SRC_PATH=$LOCAL/$DIR/$BOOT
 
 L0_PATH=.
 
 echo $SRC_PATH
 #scp jintack@jintack.cs.columbia.edu:$SRC_PATH/$IMAGE  .
+cp $SRC_PATH/Image .
+
 sudo mount $L0_PATH/L0.img l0_mnt
 md5sum $IMAGE
 sudo cp $IMAGE l0_mnt/srv/vm/Image
-md5sum l0_mnt/srv/vm/Image
+sudo md5sum l0_mnt/srv/vm/Image
 sudo umount l0_mnt
