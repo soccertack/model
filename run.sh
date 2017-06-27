89fbb923052aaee5290b807ce7b01e0f4e377d87
Jintack Lim <jintack.lim@linaro.org>
2017-06-27 18:47:54 -0500

    Unmount all guest file system before starting the model
    Signed-off-by: Jintack Lim <jintack.lim@linaro.org>

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 ARMLMD_LICENSE_FILE=8224@127.0.0.1
 export ARMLMD_LICENSE_FILE
+sudo umount l2_mnt
+sudo umount l1_mnt
+sudo umount l0_mnt
 date
 /users/jintackl/linaro-v83/models/Linux64_GCC-4.7/FVP_Base_AEMv8A-AEMv8A \
 -a "cluster0.*=linux-system.axf" \
