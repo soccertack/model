bd9d0c393b609056b6719b1048b0a0c679d65357
Jintack Lim <jintack.lim@linaro.org>
2017-06-28 19:13:24 -0500

    Send telegram message
    Signed-off-by: Jintack Lim <jintack.lim@linaro.org>

---
+++ send_tg.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ssh jintack@jintack.cs.columbia.edu "/home/jintack/send_tg.sh $1"
--- smp.sh
+++ smp.sh
@@ -4,12 +4,17 @@ set timeout 1200000
 
 log_file mylog
 source start_telnet.sh
-sleep 1
+exec ./send_tg.sh start_telnet
 send "\n"
 expect -re "L0.*# "
+exec ./send_tg.sh start_smp_L1
 send "./run.sh 2\n"
 expect -re "L1.*# "
+exec ./send_tg.sh L1_up
 send "cd vm\n"
 expect -re "L1.*# "
+exec ./send_tg.sh start_smp_L2
 send "./run-guest.sh -c 2\n"
 expect -re "L2.*# "
+exec ./send_tg.sh L2_up
+send "\n"
